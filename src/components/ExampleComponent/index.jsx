b763520378daca6c0d09e1469c904a434acc46c7
Everaldo Matias <matiaseveraldo@gmail.com>
2024-06-12 19:00:36 -0300

    Restart repository files

---
+++ src/components/ExampleComponent/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+
+const ExampleComponent = () => {
+    return (
+        <div className="example-component">
+            <h1>Hello, Vite and React!</h1>
+        </div>
+    )
+}
+
+export default ExampleComponent
